8d34a0059f2fc761d7a7da94b19c151339993140
Andy Dao <daoa@msoe.edu>
2025-11-04 13:02:50 -0600

    Amend silly test and actually perform file replacements in angular.json

--- bus-dashboard/src/app/services/bustime.service.ts
+++ bus-dashboard/src/app/services/bustime.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of, tap } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 import { RoutesResponse, PredictionsResponse } from '../models/bustime.models';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
